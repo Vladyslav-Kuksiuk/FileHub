022de090002b0e6a74c0b726bf4f276841c2e9ad
Vladyslav Kuksiuk <vladyslav.kuksiuk@teamdev.com>
2022-09-29 17:56:19 +0300

    Add eslint to js-app.

---
+++ js-app/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+  },
+  'extends': 'google',
+  'overrides': [
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
+  },
+  'rules': {
+    'valid-jsdoc': ['error', {
+      requireParamDescription: false,
+      requireReturnDescription: false,
+      requireReturn: false,
+      prefer: {return: 'returns'},
+    }],
+    'max-len': ['error', {code: 120}],
+    'no-console': 'error',
+    'no-debugger': 'error',
+  },
+};
