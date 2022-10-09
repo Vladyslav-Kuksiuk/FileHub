646208cd17f862e19e4a3c5b254af89145942aff
Vladyslav Kuksiuk <vladyslav.kuksiuk@teamdev.com>
2022-10-09 20:17:17 +0300

    Split forms to components.

--- js-app/src/components/authorizationpage/index.js
+++ js-app/src/components/authorizationpage/index.js
@@ -1,38 +1,20 @@
 import {Component} from '../component.js';
-import {Button} from '../button';
-import {FormControl} from '../formcontrol';
-import {EMAIL, PASSWORD} from '../../constants.js';
-import {AuthorizationValidator} from '../../authorization/authorization-validator.js';
+import {AuthorizationForm} from '../authorizationform';
 
 /**
  * Authorization page component.
  */
 export class AuthorizationPage extends Component {
   /**
-   * Adds form controls and button to form.
+   * Adds authorization form to page.
    */
   afterRender() {
-    const buttonSlot = this.getSlot('button');
-    const button = new Button(buttonSlot);
-    button.title = 'Sign In';
-
-    const emailSlot = this.getSlot('email');
-    const emailInput = new FormControl(emailSlot);
-    emailInput.name = EMAIL;
-    emailInput.labelText = 'Email';
-    emailInput.placeholder = 'Email';
-
-    const passwordSlot = this.getSlot('password');
-    const passwordInput = new FormControl(passwordSlot);
-    passwordInput.name = PASSWORD;
-    passwordInput.labelText = 'Password';
-    passwordInput.placeholder = 'Password';
-
-    new AuthorizationValidator().addValidationToForm(button, [emailInput, passwordInput]);
+    const formSlot = this.getSlot('form');
+    new AuthorizationForm(formSlot);
   }
 
   /**
-   * Returns button's HTML as string.
+   * Returns authorization page's HTML as string.
    *
    * @returns {string}
    */
@@ -45,16 +27,7 @@ export class AuthorizationPage extends Component {
     <main class="container">
         <h1>Sign in to FileHub</h1>
         <hr class="horizontal-line">
-        <form action="" class="form-horizontal form-page">
-            ${this.addSlot('email')}
-            ${this.addSlot('password')}
-            <div class="form-group">
-                <div class="col-sm-8 col-sm-offset-4 form-row-button">
-                    ${this.addSlot('button')}
-                    <a class="form-link" href="" title="Sign Up">Don't have account?</a>
-                </div>
-            </div>
-        </form>
+        ${this.addSlot('form')}
     </main>
 </div>
     `;
---
+++ js-app/src/components/registrationform/index.js
@@ -0,0 +1,60 @@
+import {Component} from '../component.js';
+import {Button} from '../button';
+import {FormControl} from '../formcontrol';
+import {CONFIRM_PASSWORD, EMAIL, PASSWORD} from '../../constants.js';
+import {RegistrationValidator} from '../../registration/registration-validator.js';
+
+/**
+ * Authorization page component.
+ */
+export class RegistrationForm extends Component {
+  /**
+   * Adds form controls and button to form.
+   */
+  afterRender() {
+    const buttonSlot = this.getSlot('button');
+    const button = new Button(buttonSlot);
+    button.title = 'Sign Up';
+
+    const emailSlot = this.getSlot('email');
+    const emailInput = new FormControl(emailSlot);
+    emailInput.name = EMAIL;
+    emailInput.labelText = 'Email';
+    emailInput.placeholder = 'Email';
+
+    const passwordSlot = this.getSlot('password');
+    const passwordInput = new FormControl(passwordSlot);
+    passwordInput.name = PASSWORD;
+    passwordInput.labelText = 'Password';
+    passwordInput.placeholder = 'Password';
+
+    const confirmSlot = this.getSlot('confirm-password');
+    const confirmInput = new FormControl(confirmSlot);
+    confirmInput.name = CONFIRM_PASSWORD;
+    confirmInput.labelText = 'Confirm Password';
+    confirmInput.placeholder = 'Confirm Password';
+
+    new RegistrationValidator().addValidationToForm(button, [emailInput, passwordInput, confirmInput]);
+  }
+
+  /**
+   * Returns registration form's HTML as string.
+   *
+   * @returns {string}
+   */
+  markup() {
+    return `
+        <form action="" class="form-horizontal form-page">
+            ${this.addSlot('email')}
+            ${this.addSlot('password')}
+            ${this.addSlot('confirm-password')}
+            <div class="form-group">
+                <div class="col-sm-8 col-sm-offset-4 form-row-button">
+                    ${this.addSlot('button')}
+                     <a class="form-link" href="" title="Sign In">Already have an account?</a>
+                </div>
+            </div>
+        </form>
+    `;
+  }
+}
