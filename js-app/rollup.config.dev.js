d3cfa0948fb933799ec4f15f1f4f8a57f2945899
Hellamb <vladyslav.kuksiuk@teamdev.com>
2022-10-16 19:51:17 +0300

    Implement rollup building.

---
+++ js-app/rollup.config.dev.js
@@ -0,0 +1,36 @@
+import {nodeResolve} from '@rollup/plugin-node-resolve';
+import htmlTemplate from 'rollup-plugin-generate-html-template';
+import copy from 'rollup-plugin-copy';
+import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload';
+import hashedMapping from '@trendyminds/rollup-plugin-hashed-mapping';
+
+
+export default {
+  input: 'src/index.js',
+  output: {
+    dir: 'dist',
+    entryFileNames: '[name]-[hash].js',
+  },
+  plugins: [
+    nodeResolve(),
+    htmlTemplate({
+      template: 'index.html',
+      target: 'dist/index.html',
+    }),
+    copy({
+      targets: [
+        {src: 'static/styles', dest: 'dist/static'},
+        {src: 'static/images', dest: 'dist/static'},
+        {src: 'static/fonts', dest: 'dist/static'},
+      ],
+    }),
+    serve({
+      open: true,
+      port: 3000,
+      contentBase: 'dist',
+    }),
+    livereload(),
+    hashedMapping(),
+  ],
+};
