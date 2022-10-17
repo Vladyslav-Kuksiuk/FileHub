36723c8c940a6d29b3ae3e76d18d9c27557a2c81
Vladyslav Kuksiuk <vladyslav.kuksiuk@teamdev.com>
2022-10-17 23:03:24 +0300

    Refactor router.

--- js-app/src/router.js
+++ js-app/src/router.js
@@ -2,43 +2,124 @@
  * Class to provide routing.
  */
 export class Router {
+  #rootElement;
   #pages = {};
-  #defaultPageCreator;
-  #error404PageCreator;
+  #homePageName;
+  #errorPageCreator;
 
   /**
-   * @param {function(HTMLElement)} pageCreator
+   * @param {HTMLElement} rootElement
+   * @param {Object} pages
+   * @param {string} homePageName
+   * @param {function(HTMLElement)} errorPageCreator
    */
-  set defaultPage(pageCreator) {
-    this.#defaultPageCreator = pageCreator;
+  constructor(rootElement, pages, homePageName, errorPageCreator) {
+    this.#rootElement = rootElement;
+    this.#pages = pages;
+    this.#homePageName = homePageName;
+    this.#errorPageCreator = errorPageCreator;
+
+    window.addEventListener('hashchange', () => {
+      this.#renderPage(window.location.hash.replace('#', ''));
+    });
+
+    this.#renderPage(window.location.hash.replace('#', ''));
   }
 
   /**
-   * @param {function(HTMLElement)} pageCreator
-   */
-  set error404Page(pageCreator) {
-    this.#error404PageCreator = pageCreator;
-  }
-
-  /**
+   * Renders page in root element.
    * @param {string} pageName
-   * @param {function(HTMLElement)} pageCreator
    */
-  addPage(pageName, pageCreator) {
-    this.#pages[pageName] = pageCreator;
-  }
-
-  /**
-   * @param {string} pageName
-   * @returns {function(HTMLElement)}
-   */
-  getPage(pageName) {
-    if (!!this.#pages[pageName]) {
-      return this.#pages[pageName];
-    } else if (pageName === '') {
-      return this.#defaultPageCreator;
+  #renderPage(pageName) {
+    this.#rootElement.innerHTML = '';
+    if (pageName === '') {
+      this.#pages[this.#homePageName](this.#rootElement);
+    } else if (this.#pages[pageName]) {
+      this.#pages[pageName](this.#rootElement);
     } else {
-      return this.#error404PageCreator;
+      this.#errorPageCreator(this.#rootElement);
     }
+  }
+
+  /**
+   *
+   * @returns {RouterBuilder}
+   */
+  static getBuilder() {
+    return new RouterBuilder();
+  }
+
+  /**
+   * Redirects to given page.
+   *
+   * @param {string} pageName
+   */
+  redirect(pageName) {
+    window.location.hash = pageName;
+  }
+}
+
+/**
+ * Builder for {@link Router}.
+ */
+class RouterBuilder {
+  #rootElement;
+  #pages = {};
+  #errorPageCreator;
+  #homePageName;
+
+  /**
+   * Adds root element to render components in it.
+   *
+   * @param {HTMLElement} element
+   * @returns {RouterBuilder}
+   */
+  addRootElement(element) {
+    this.#rootElement = element;
+    return this;
+  }
+
+  /**
+   * Adds page to router.
+   *
+   * @param {string} name
+   * @param {function(HTMLElement)} creator
+   * @returns {RouterBuilder}
+   */
+  addPage(name, creator) {
+    this.#pages[name] = creator;
+    return this;
+  }
+
+  /**
+   * Adds error404 page to router.
+   *
+   * @param {function(HTMLElement)} creator
+   * @returns {RouterBuilder}
+   */
+  addErrorPage(creator) {
+    this.#errorPageCreator = creator;
+    return this;
+  }
+
+  /**
+   * Adds home page name to router.
+   *
+   * @param {string} name
+   * @returns {RouterBuilder}
+   */
+  addHomePageName(name) {
+    this.#homePageName = name;
+    return this;
+  }
+
+  /**
+   * @returns {Router}
+   */
+  build() {
+    return new Router(this.#rootElement,
+        this.#pages,
+        this.#homePageName,
+        this.#errorPageCreator);
   }
 }
