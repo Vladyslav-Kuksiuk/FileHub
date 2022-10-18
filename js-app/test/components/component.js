0cdc086e90eab8e8ae31dabbf36558c9b14969c1
Vladyslav Kuksiuk <vladyslav.kuksiuk@teamdev.com>
2022-10-18 12:49:22 +0300

    Add component tests.

---
+++ js-app/test/components/component.js
@@ -0,0 +1,69 @@
+import {Component} from '../../src/components/component.js';
+
+const {module, test} = QUnit;
+
+class TestComponent extends Component{
+  executeSequence;
+
+  constructor(parent) {
+    super(parent);
+    this.executeSequence = ['constructor']
+    this.init()
+  }
+
+  init() {
+    this.executeSequence.push('init')
+    super.init();
+  }
+
+  render() {
+    this.executeSequence.push('render')
+    super.render();
+  }
+
+  afterRender() {
+    this.executeSequence.push('afterRender')
+    super.afterRender();
+  }
+
+  markup() {
+    this.executeSequence
+    return '<slot><p>test</p></slot>';
+  }
+}
+
+module('Component', () => {
+  test(`Lifecycle`, function(assert) {
+    assert.expect(2);
+    const fixture = document.getElementById('qunit-fixture');
+    const testComponent = new TestComponent(fixture);
+
+    assert.strictEqual(testComponent.executeSequence.join(),
+      ['constructor','init','render','afterRender'].join(),
+      'Should return executing sequence of component creating.');
+
+    testComponent.render()
+    assert.strictEqual(testComponent.executeSequence.join(),
+      ['constructor','init','render','afterRender','render','afterRender'].join(),
+      'Should return executing sequence of component creating.');
+  });
+
+  test(`Markup insertion`, function(assert) {
+    assert.expect(3);
+    const fixture = document.getElementById('qunit-fixture');
+
+    assert.strictEqual(fixture.querySelectorAll('p').length,
+      0,
+      'Should return number of p-element before component creating.');
+
+    new TestComponent(fixture);
+
+    assert.strictEqual(fixture.querySelectorAll('p').length,
+      1,
+      'Should return number of p-element after component creating.');
+    assert.strictEqual(fixture.querySelector('p').innerText,
+      'test',
+      'Should return innerText of p-element after component creating.');
+
+  });
+});
--- js-app/test/index.js
+++ js-app/test/index.js
@@ -1,4 +1,5 @@
 import './validation/value-validations.js';
+import './components/component.js';
 import './components/button.js';
 import './components/link.js';
 import './components/form.js';
