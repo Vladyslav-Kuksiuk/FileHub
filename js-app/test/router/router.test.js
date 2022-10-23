b02cec49dbf281d2c50f0c9c95a284081178183f
Hellamb <vladyslav.kuksiuk@teamdev.com>
2022-10-23 23:19:34 +0300

    Improve router tests.

--- js-app/test/router/router.test.js
+++ js-app/test/router/router.test.js
@@ -2,26 +2,53 @@ import {RouterConfigBuilder} from '../../src/router/router-config';
 import {Router} from '../../src/router/router';
 
 describe('Router', () => {
+  const routerConfig = new RouterConfigBuilder()
+      .addHomeRoutePath('login')
+      .addErrorRoute(() => {
+        document.body.textContent = 'error';
+      })
+      .addRoute('register', () => {
+        document.body.textContent = 'register';
+      })
+      .addRoute('login', () => {
+        document.body.textContent = 'login';
+      }).build();
+
+  test(`Should redirect to login->register->error`, function(done) {
+    expect.assertions(6);
+
+    window.location.hash = '';
+
+    const router = new Router(routerConfig);
+
+    expect(document.body.textContent).toBe('login');
+    expect(window.location.hash).toBe('#login');
+
+    router.redirect('register');
+
+    setTimeout(()=>{
+      expect(document.body.textContent).toBe('register');
+      expect(window.location.hash).toBe('#register');
+
+      router.redirect('notExistingPage');
+
+      setTimeout(()=>{
+        expect(document.body.textContent).toBe('error');
+        expect(window.location.hash).toBe('#notExistingPage');
+        done();
+      });
+    });
+  });
+
   test(`Should redirect to login->register->error`, function(done) {
     expect.assertions(3);
 
     window.location.hash = '';
 
-    const routerConfig = new RouterConfigBuilder()
-        .addHomeRoutePath('login')
-        .addErrorRoute(() => {
-          document.body.textContent = 'error';
-        })
-        .addRoute('register', () => {
-          document.body.textContent = 'register';
-        })
-        .addRoute('login', () => {
-          document.body.textContent = 'login';
-        }).build();
-
     const router = new Router(routerConfig);
 
     expect(document.body.textContent).toBe('login');
+    window.location.hash = '#login';
 
     router.redirect('register');
 
